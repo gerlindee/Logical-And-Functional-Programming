8f6bd5a62e6ec7ed19eed887efe1ba5e0ff03296
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2019-01-07 01:18:14 +0200

    Update 15.lisp

--- misc_problems/15.lisp
+++ misc_problems/15.lisp
@@ -1,3 +1,14 @@
+; not using mapcar
+
+(defun square_list_1(list)
+    (cond
+        ((null list) nil)
+        
+        (T
+         (cons (* (car list) (car list)) (square_list_1 (cdr list))))))
+
+; using mapcar and another function
+
 (defun square_up(number)
     (* number number))
 
@@ -7,3 +18,7 @@
         
         (T
          (mapcar 'square_up list))))
+
+; using mapcar and a lamnda expression
+
+
